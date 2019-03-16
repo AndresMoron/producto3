84e8ed0976462ef954c58da30d7b07095eb5dcdf
Andres Moron <andresmoron622@gmail.com>
2019-03-16 11:18:25 -0400

    primer commit

---
+++ Producto3Backend/Producto3Backend/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Producto3Backend.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Producto3Backend.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ Producto3Backend/Producto3Backend/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+
+namespace Producto3Backend.Models
+{
+  using System.ComponentModel.DataAnnotations;
+public class Producto
+  
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name="Nombre del Producto")]
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
